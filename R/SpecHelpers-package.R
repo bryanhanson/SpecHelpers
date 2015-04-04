e438631aee11b225340ea8eb304e2e3d9a53d4db
Bryan Hanson <hanson@depauw.edu>
2015-04-04 19:22:47 -0400

    First commit: getting everything set up

---
+++ R/SpecHelpers-package.R
@@ -0,0 +1,19 @@
+
+
+#' SpecHelpers: Spectroscopy related utilities
+#' 
+#' Utility functions for spectroscopy.  1. Functions to simulate spectra for
+#' use in teaching or testing.  2.  Functions to process files created by
+#' LoggerPro and SpectraSuite software. 
+#' 
+#' @name SpecHelpers-package
+#' @aliases SpecHelpers-package SpecHelpers
+#' @docType package
+#' @author Bryan A. Hanson, DePauw University, Greencastle Indiana USA
+#' 
+#' Maintainer: Bryan A. Hanson \email{hanson@@depauw.edu}
+#' @keywords package
+NULL
+
+
+
